e35cd51506f63c9e3397e4825b25e6fe7af8d687
Jeová de Sousa Barbosa <jeovasousabarbosassj@gmail.com>
2025-01-30 15:32:51 -0300

    alterações no layout e models

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -7,4 +7,5 @@ def posts_list(request):
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
 def posts_page(request,slug):
-    return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
