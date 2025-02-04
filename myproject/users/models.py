3f05afff2bc2bd855b8e98461c45fc8c4d4efa08
Jeová de Sousa Barbosa <jeovasousabarbosassj@gmail.com>
2025-02-04 11:31:29 -0300

    ultima aula do curso

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -2,15 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
-    name = models.CharField(max_length=75)
-    age = models.IntegerField()
-    gender = {
-        "M": "Male",
-        "F": "Famale",
-        "O": "Other"
-    }
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.users, name="users"),
     path('register/', views.register_view, name="register"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout")
 ]
